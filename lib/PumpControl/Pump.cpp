777bfc6747ac7d7e0acadc2597489d600949cc9e
Evgeniy Karpenko <e.karpenko@icol.com>
2022-06-16 09:25:05 +0300

    Added pump device

---
+++ lib/PumpControl/Pump.cpp
@@ -0,0 +1,51 @@
+#include "Pump.hpp"
+#include <Arduino.h>
+#include <Message.hpp>
+
+namespace device
+{
+
+Pump::Pump(uint16_t serviceId, gpio_num_t pin, xQueueHandle messageQueue, uint16_t timeOn) :
+    m_serviceId(serviceId), m_pumpGpio(pin), m_messageQueue(messageQueue) 
+{
+    m_onTimer = xTimerCreate("PumpOnTimer", timeOn / portTICK_PERIOD_MS, pdFALSE, this, timerOnHandler);
+    pinMode(m_pumpGpio, OUTPUT);
+    digitalWrite(m_pumpGpio, LOW);
+}
+
+void Pump::startPuls() noexcept
+{
+    digitalWrite(m_pumpGpio, HIGH);
+    xTimerStart(m_onTimer, 0);
+}
+
+void Pump::off() noexcept
+{
+    digitalWrite(m_pumpGpio, LOW);
+    if (xTimerIsTimerActive(m_onTimer))
+    {
+        xTimerStop(m_onTimer, 0);
+    }
+
+    utils::Message message
+    {
+        .serviceId = m_serviceId,
+        .data = {0},
+        .size = 1
+    };
+    xQueueSend(m_messageQueue, &message, 0);
+}
+
+void Pump::changeOnTime(uint16_t timeOn) noexcept
+{
+    off();
+    xTimerChangePeriod(m_onTimer, timeOn / portTICK_PERIOD_MS, 0);
+}
+
+void Pump::timerOnHandler(void* pumpPtr) noexcept
+{
+    auto pump = reinterpret_cast<Pump*>(pumpPtr);
+    pump->off();
+}
+
+}
---
+++ lib/PumpControl/Pump.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <freertos/FreeRTOS.h>
+#include <freertos/timers.h>
+#include <driver/gpio.h>
+#include <freertos/queue.h>
+
+namespace device
+{
+
+class Pump
+{
+   public:
+    Pump(uint16_t service, gpio_num_t pin, xQueueHandle messageQueue, uint16_t timeOn);
+    ~Pump() = default;
+
+   public:
+    void startPuls() noexcept;
+
+    void changeOnTime(uint16_t timeOn) noexcept;
+
+    void off() noexcept;
+
+   private:
+    static void timerOnHandler(void* pumpPtr) noexcept;
+
+   private: 
+    uint16_t const m_serviceId;
+    gpio_num_t const m_pumpGpio;
+    xQueueHandle m_messageQueue;
+    xTimerHandle m_onTimer;
+};
+
+}
